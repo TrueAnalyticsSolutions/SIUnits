60c10f3f2f209f51a2b3c9477d1a1d82cf9d8671
tbm0115 <tbm0115@gmail.com>
2019-12-28 14:40:28 -0800

    Added package

---
+++ SIUnitsCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIUnitsCore {
+    static class Program {
+        static void Main(string[] args) {
+            IEnumerable<SIUnits.Abstracts.BaseUnit> allUnits = SIUnits.UnitManager.GetUnits();
+
+            Type typDerived = typeof(SIUnits.DerivedUnit);
+            Type typFactored = typeof(SIUnits.FactoredUnit);
+
+            string strConvert = "33lb"; // How much the average Koala weighs
+            string strTargetUnit = "kg"; // Kilograms
+
+            decimal decKoalaWeight = (decimal)SIUnits.UnitManager.Convert(strConvert, strTargetUnit); // Cast because errors in conversion may return null
+
+            Console.WriteLine("A {0} Koala weighs about {1}{2}", strConvert, Math.Round(decKoalaWeight, 2).ToString(), strTargetUnit);
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
